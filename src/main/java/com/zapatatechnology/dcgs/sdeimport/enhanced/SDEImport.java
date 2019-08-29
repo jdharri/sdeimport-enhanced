7607650a16bc48d6f5f396be40fe1b6ceb0bfe60
Joel <joel@zapatatechnology.com>
2019-08-29 09:11:30 -0400

    init

--- src/main/java/com/zapatatechnology/dcgs/sdeimport/enhanced/SDEImport.java
+++ src/main/java/com/zapatatechnology/dcgs/sdeimport/enhanced/SDEImport.java
@@ -5,12 +5,43 @@
  */
 package com.zapatatechnology.dcgs.sdeimport.enhanced;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author zapataai
  */
 public class SDEImport {
-    public static void main(String[] args){
-        System.out.println("test");
+
+    public static void main(String[] args) {
+        try {
+            WatchService watchService = FileSystems.getDefault().newWatchService();
+            Path path = Paths.get("/home/zapataai");
+            path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
+            WatchKey key;
+            while ((key = watchService.take()) != null) {
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    System.out.println(
+                            "Event kind:" + event.kind()
+                            + ". File affected: " + event.context() + ".");
+                }
+                key.reset();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(SDEImport.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(SDEImport.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 }
--- src/main/java/com/zapatatechnology/dcgs/sdeimport/enhanced/SDEImportProperties.java
+++ src/main/java/com/zapatatechnology/dcgs/sdeimport/enhanced/SDEImportProperties.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.zapatatechnology.dcgs.sdeimport.enhanced;
+
+import java.util.Properties;
 
 /**
  *
  * @author zapataai
  */
-public class SDEImportProperties {
+public class SDEImportProperties extends Properties{
+    private String sdeHostname;
     
 }
