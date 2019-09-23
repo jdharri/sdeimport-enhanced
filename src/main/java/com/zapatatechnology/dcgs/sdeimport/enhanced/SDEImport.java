61241f62d370f7da5911afebca67f86370dc4eee
joel <joel@2017JHARRI4W4QB>
2019-09-23 15:04:21 -0400

    generating properties file

--- src/main/java/com/zapatatechnology/dcgs/sdeimport/enhanced/SDEImport.java
+++ src/main/java/com/zapatatechnology/dcgs/sdeimport/enhanced/SDEImport.java
@@ -1,7 +1,11 @@
-
 package com.zapatatechnology.dcgs.sdeimport.enhanced;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,7 +13,7 @@ import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
-import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,7 +26,7 @@ public class SDEImport {
     public static void main(String[] args) {
         try {
             WatchService watchService = FileSystems.getDefault().newWatchService();
-            Path path = Paths.get("/incoming");
+            Path path = Paths.get(writeFile("test"));
             path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
             WatchKey key;
             while ((key = watchService.take()) != null) {
@@ -30,7 +34,7 @@ public class SDEImport {
                     System.out.println(
                             "Event kind:" + event.kind()
                             + ". File affected: " + event.context() + ".");
-                    if(event.kind().equals(StandardWatchEventKinds.ENTRY_CREATE)){
+                    if (event.kind().equals(StandardWatchEventKinds.ENTRY_CREATE)) {
                         System.out.println("new file created");
                     }
                 }
@@ -44,121 +48,111 @@ public class SDEImport {
 
     }
 
-   /** public static synchronized String importProcess(String input) {
-        ProcessBuilder pb = new ProcessBuilder("echo " + input);
+    public static String writeFile(String value) throws IOException {
+        String PATH = "/";
+        String directoryName = "/incoming";
+        String fileName = "sdeimport.properties";
 
-        pb.redirectErrorStream(true);
-        Process p = pb.start();
-    }
+        File directory = new File(directoryName);
 
-    String prefix = "";
+        if (!directory.exists()) {
+            directory.mkdir();
+            // If you require it to make the entire directory path including parents,
+            // use directory.mkdirs(); here instead.
+        }
 
-    if(props.getUsingCYGWIN() == true){
-prefix = "cmd,/c,":
-}
-
-String searchFields = "";
-    String operations = "";
-    if(deleteFile
-
-    
-        ){
-searchFields = props.getMainIdColumn(this.i);
-        operations = "delete";
-    }
-
-    
-        else {
-searchFields = props.getKeyFields(this.i);
-        operations = "update_else_insert";
-    }
-    final String pbCommands = prefix + props.getImportedCommand()
-            + ",-o,"
-            + operations
-            + ",-t,"
-            + props.getTable(this.i)
-            + f.getAbsolutePath()
-            + sdeHost
-            + ",-u,"
-            + props.getUser(this.i)
-            + ",-p,"
-            + props.getPasswd(this.i)
-            + ",-K,"
-            + searchFields;
-
-    final ProcessBuilder pb = new ProcessBuilder(pbCommands.split(","));
-
-    pb.redirectErrorStream (
+        File file = new File(directoryName + "/" + fileName);
+        try {
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
             
-    true);
-final Process p = pb.start();
-    final java.io.InputStream expout = p.getInputStream();
-    final int exitcode = p.waitFor();
-    if(expout
-
-    
-        != null) {
-final byte[] buf = new byte[512];
-        int len = 0;
-        while ((len = expout.read(buf)) >= 0) {
-//log
+            SDEImportProperties props = new SDEImportProperties();
+            FileOutputStream out = new FileOutputStream(file);
+            
+            Field[] fields =  SDEImportProperties.class.getDeclaredFields();
+            
+            for(int i =0; i<fields.length;i++){
+                System.out.println("key: "+fields[i].getName());
+                String fieldName = fields[i].getName();
+                props.setProperty(fieldName, "");
         }
-        if ((exitcode == 0) || (exitcode == 134)) {
-            if (!this.haveAllFiles(f, true)) {
-                continue;
-            }
-        } else {
-//log
+           props.store(out, fileName);
+           // bw.write(value);
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
         }
-        Thread.sleep(1000);
+        return file.getParent();
+    }
+    /**
+     * public static synchronized String importProcess(String input) {
+     * ProcessBuilder pb = new ProcessBuilder("echo " + input);
+     *
+     * pb.redirectErrorStream(true); Process p = pb.start(); }
+     *
+     * String prefix = "";
+     *
+     * if(props.getUsingCYGWIN() == true){ prefix = "cmd,/c,": }
+     *
+     * String searchFields = ""; String operations = ""; if(deleteFile
+     *
+     *
+     * ){ searchFields = props.getMainIdColumn(this.i); operations = "delete"; }
+     *
+     *
+     * else { searchFields = props.getKeyFields(this.i); operations =
+     * "update_else_insert"; } final String pbCommands = prefix +
+     * props.getImportedCommand() + ",-o," + operations + ",-t," +
+     * props.getTable(this.i) + f.getAbsolutePath() + sdeHost + ",-u," +
+     * props.getUser(this.i) + ",-p," + props.getPasswd(this.i) + ",-K," +
+     * searchFields;
+     *
+     * final ProcessBuilder pb = new ProcessBuilder(pbCommands.split(","));
+     *
+     * pb.redirectErrorStream (
+     *
+     * true); final Process p = pb.start(); final java.io.InputStream expout =
+     * p.getInputStream(); final int exitcode = p.waitFor(); if(expout
+     *
+     *
+     * != null) { final byte[] buf = new byte[512]; int len = 0; while ((len =
+     * expout.read(buf)) >= 0) { //log } if ((exitcode == 0) || (exitcode ==
+     * 134)) { if (!this.haveAllFiles(f, true)) { continue; } } else { //log }
+     * Thread.sleep(1000);
+     *
+     *
+     *
+     *
+     *
+     *
+     * private boolean haveAllFiles(final File FileName, final Boolean
+     * deleteFiles) { final File dir = new File(props.getInDir(this.i)); if
+     * ((dir == null) || !dir.exists() || !dir.isDirectory()) { //log } else {
+     * File[] files = null; files = dir.listFiles();
+     *
+     * if ((files != null) && (files.length > 0)) { //log final
+     * FilePriorityQueue fileQueue = new FilePriorityQueue(files.length);
+     *
+     * for (final File f : files) { fileQueue.offer(f); }
+     *
+     * this.FoundFilesArray = new ArayList<File>(); int numFiles = 0; int
+     * currentFileNum = 0; int counter = 0;
+     *
+     * while (!fileQueue.isEmpty()) { final File f = fileQueue.poll();
+     *
+     * if (!f.isFile()) { continue; } //log
+     *
+     * String getFileName = FileNme.getName(); getFileName =
+     * getFileName.substring(0, getFileName.length() - 3); final int FoundFile =
+     * f.getName().indexOf(getFileName); final String currentFileName =
+     * f.getName();
+     *
+     * if ((FoundFile != -1)) { try { currentFileNum = Integer.parseInt } } }
+     *
+     * }
+     *
+     */
 
-
-
-
-    
-
-    private boolean haveAllFiles(final File FileName, final Boolean deleteFiles) {
-        final File dir = new File(props.getInDir(this.i));
-        if ((dir == null) || !dir.exists() || !dir.isDirectory()) {
-//log
-        } else {
-            File[] files = null;
-            files = dir.listFiles();
-
-            if ((files != null) && (files.length > 0)) {
-//log
-                final FilePriorityQueue fileQueue = new FilePriorityQueue(files.length);
-
-                for (final File f : files) {
-                    fileQueue.offer(f);
-                }
-
-                this.FoundFilesArray = new ArayList<File>();
-                int numFiles = 0;
-                int currentFileNum = 0;
-                int counter = 0;
-
-                while (!fileQueue.isEmpty()) {
-                    final File f = fileQueue.poll();
-
-                    if (!f.isFile()) {
-                        continue;
-                    }
-//log
-
-                    String getFileName = FileNme.getName();
-                    getFileName = getFileName.substring(0, getFileName.length() - 3);
-                    final int FoundFile = f.getName().indexOf(getFileName);
-                    final String currentFileName = f.getName();
-
-                    if ((FoundFile != -1)) {
-                        try {
-                            currentFileNum = Integer.parseInt
-                        }
-                    }
-                }
-                
-            }
-           **/
-
-        }
+}
--- src/main/java/com/zapatatechnology/dcgs/sdeimport/enhanced/SDEImportProperties.java
+++ src/main/java/com/zapatatechnology/dcgs/sdeimport/enhanced/SDEImportProperties.java
@@ -9,24 +9,24 @@ import java.util.Properties;
  */
 public class SDEImportProperties extends Properties{
         
-   public static final String CGWIN = "cgwyn";
-   public static final String DELETE_FILES = "deleteFiles";
-   public static final String DRIVER_SDE = "driverSDE";
-   public static final String IMPORT_COMMAND = "importCommand";
-   public static final String IN_DIR = "inDir";
-   public static final String KEY_FIELDS = "keyFields";
-   public static final String LAYER_NAME = "layerName";
-   public static final String MAIN_ID_COLUMN = "mainIDColumn";
-   public static final String MIN_FILE_LATENCY = "minFileLatency";
-   public static final String NUM_IMPORTS = "numImports";
-   public static final String PASSWD_SDE = "passwdSDE";
-   public static final String SDE_DATABASE = "SDEDatabase";
-   public static final String SDE_HOST = "SDEHost";
-   public static final String SDE_PORT = "SDEPort";
-   public static final String SLEEP_TIME = "sleepTime";
-   public static final String TABLE_NAME = "tableName";
-   public static final String URL_SDE = "urlSDE";
-   public static final String USER_SDE = "userSDE";
+   public   String CGWIN = "cgwyn";
+   public   String DELETE_FILES = "deleteFiles";
+   public   String DRIVER_SDE = "driverSDE";
+   public   String IMPORT_COMMAND = "importCommand";
+   public   String IN_DIR = "inDir";
+   public   String KEY_FIELDS = "keyFields";
+   public   String LAYER_NAME = "layerName";
+   public   String MAIN_ID_COLUMN = "mainIDColumn";
+   public   String MIN_FILE_LATENCY = "minFileLatency";
+   public   String NUM_IMPORTS = "numImports";
+   public   String PASSWD_SDE = "passwdSDE";
+   public   String SDE_DATABASE = "SDEDatabase";
+   public   String SDE_HOST = "SDEHost";
+   public   String SDE_PORT = "SDEPort";
+   public   String SLEEP_TIME = "sleepTime";
+   public   String TABLE_NAME = "tableName";
+   public   String URL_SDE = "urlSDE";
+   public   String USER_SDE = "userSDE";
 
     public Properties getDefaults() {
         return defaults;
@@ -34,6 +34,150 @@ public class SDEImportProperties extends Properties{
 
     public void setDefaults(Properties defaults) {
         this.defaults = defaults;
+    }
+
+    public String getCGWIN() {
+        return CGWIN;
+    }
+
+    public void setCGWIN(String CGWIN) {
+        this.CGWIN = CGWIN;
+    }
+
+    public String getDELETE_FILES() {
+        return DELETE_FILES;
+    }
+
+    public void setDELETE_FILES(String DELETE_FILES) {
+        this.DELETE_FILES = DELETE_FILES;
+    }
+
+    public String getDRIVER_SDE() {
+        return DRIVER_SDE;
+    }
+
+    public void setDRIVER_SDE(String DRIVER_SDE) {
+        this.DRIVER_SDE = DRIVER_SDE;
+    }
+
+    public String getIMPORT_COMMAND() {
+        return IMPORT_COMMAND;
+    }
+
+    public void setIMPORT_COMMAND(String IMPORT_COMMAND) {
+        this.IMPORT_COMMAND = IMPORT_COMMAND;
+    }
+
+    public String getIN_DIR() {
+        return IN_DIR;
+    }
+
+    public void setIN_DIR(String IN_DIR) {
+        this.IN_DIR = IN_DIR;
+    }
+
+    public String getKEY_FIELDS() {
+        return KEY_FIELDS;
+    }
+
+    public void setKEY_FIELDS(String KEY_FIELDS) {
+        this.KEY_FIELDS = KEY_FIELDS;
+    }
+
+    public String getLAYER_NAME() {
+        return LAYER_NAME;
+    }
+
+    public void setLAYER_NAME(String LAYER_NAME) {
+        this.LAYER_NAME = LAYER_NAME;
+    }
+
+    public String getMAIN_ID_COLUMN() {
+        return MAIN_ID_COLUMN;
+    }
+
+    public void setMAIN_ID_COLUMN(String MAIN_ID_COLUMN) {
+        this.MAIN_ID_COLUMN = MAIN_ID_COLUMN;
+    }
+
+    public String getMIN_FILE_LATENCY() {
+        return MIN_FILE_LATENCY;
+    }
+
+    public void setMIN_FILE_LATENCY(String MIN_FILE_LATENCY) {
+        this.MIN_FILE_LATENCY = MIN_FILE_LATENCY;
+    }
+
+    public String getNUM_IMPORTS() {
+        return NUM_IMPORTS;
+    }
+
+    public void setNUM_IMPORTS(String NUM_IMPORTS) {
+        this.NUM_IMPORTS = NUM_IMPORTS;
+    }
+
+    public String getPASSWD_SDE() {
+        return PASSWD_SDE;
+    }
+
+    public void setPASSWD_SDE(String PASSWD_SDE) {
+        this.PASSWD_SDE = PASSWD_SDE;
+    }
+
+    public String getSDE_DATABASE() {
+        return SDE_DATABASE;
+    }
+
+    public void setSDE_DATABASE(String SDE_DATABASE) {
+        this.SDE_DATABASE = SDE_DATABASE;
+    }
+
+    public String getSDE_HOST() {
+        return SDE_HOST;
+    }
+
+    public void setSDE_HOST(String SDE_HOST) {
+        this.SDE_HOST = SDE_HOST;
+    }
+
+    public String getSDE_PORT() {
+        return SDE_PORT;
+    }
+
+    public void setSDE_PORT(String SDE_PORT) {
+        this.SDE_PORT = SDE_PORT;
+    }
+
+    public String getSLEEP_TIME() {
+        return SLEEP_TIME;
+    }
+
+    public void setSLEEP_TIME(String SLEEP_TIME) {
+        this.SLEEP_TIME = SLEEP_TIME;
+    }
+
+    public String getTABLE_NAME() {
+        return TABLE_NAME;
+    }
+
+    public void setTABLE_NAME(String TABLE_NAME) {
+        this.TABLE_NAME = TABLE_NAME;
+    }
+
+    public String getURL_SDE() {
+        return URL_SDE;
+    }
+
+    public void setURL_SDE(String URL_SDE) {
+        this.URL_SDE = URL_SDE;
+    }
+
+    public String getUSER_SDE() {
+        return USER_SDE;
+    }
+
+    public void setUSER_SDE(String USER_SDE) {
+        this.USER_SDE = USER_SDE;
     }
    
    
